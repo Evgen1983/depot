ebcca1cb40dc4db9bf56e485cceb7f95cf0ee690
Evgen1983 <ebaranov1983@gmail.com>
2015-11-06 15:55:01 +0300

    order

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
  
 
+  resources :orders
   resources :line_items
   resources :carts
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   
    root 'store#index', as: 'store'
 
